dc73bb3ba2db4ecca06e4f1c1ea3dc7e3cb9e78d
bilalnaseem <bilal9627862012@gmail.com>
2017-01-19 10:14:48 +0530

    add admin setting

---
+++ application/views/admin/main.php
@@ -0,0 +1,15 @@
+ <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>Home</h2>   
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+              
+                 <!-- /. ROW  -->           
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
---
+++ application/views/admin/template.php
@@ -0,0 +1,6 @@
+<?php
+
+$this->load->view('admin/header');
+$this->load->view($content);
+$this->load->view('admin/footer');
+?>
